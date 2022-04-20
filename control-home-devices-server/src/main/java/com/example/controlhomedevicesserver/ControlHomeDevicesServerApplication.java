8618b0c8b7226cbe887d67bde5bccb6d176e8914
Youssef Agagg <youssefagagg@gmail.com>
2022-04-20 19:59:56 +0200

    init commit

---
+++ control-home-devices-server/src/main/java/com/example/controlhomedevicesserver/ControlHomeDevicesServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.controlhomedevicesserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlHomeDevicesServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControlHomeDevicesServerApplication.class, args);
+    }
+
+}
---
+++ control-home-devices-server/src/test/java/com/example/controlhomedevicesserver/ControlHomeDevicesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.controlhomedevicesserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlHomeDevicesServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
